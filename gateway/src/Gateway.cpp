fc94e0ee20e0d2e46e2d6dace5209f32ab3d6661
Seth Hendrick <xforever1313@aim.com>
2014-06-15 18:57:12 -0400

    Fixed (Hacked around) the strange behavior where if a UartRXEvent happens, it kicks out std::cin
    The fix is that if cin fails, then clear the fail bit, and try again.
    
    This will probably need to be fixed at some point, especially since there will be random Rx's from the field, but for now....

--- gateway/src/Gateway.cpp
+++ gateway/src/Gateway.cpp
@@ -40,26 +40,24 @@ void Gateway::start() {
         m_uart->open("/dev/ttyAMA0");
         m_recvThread->start();
 
-        /*
         std::string input = "";
         std::string promptMessage = "\nEnter a number:\n\t1.  Uart Tx\n\t0.  Exit\n>";
 
         while (input != "0"){
-            input = Common::IO::InputReader::PromptForInput(promptMessage, *m_input, *m_output);
+            if (m_input->fail()) {
+                m_input->clear();
+                input = Common::IO::InputReader::PromptForInput("\n>", *m_input, *m_output);
+            }
+            else {
+                input = Common::IO::InputReader::PromptForInput(promptMessage, *m_input, *m_output);
+            }
 
-            if (input == "1") {
+            if ((input == "1") && !m_input->fail()) {
                 std::string promptMessage2 = "\nEnter a message to send:\n>";
                 std::string msg = Common::IO::InputReader::PromptForInput(promptMessage2, *m_input, *m_output);
                 m_eventExecutor->addEvent(std::shared_ptr<Common::EventInterface>(new UartTxEvent(msg, m_uart)));
             }
-
         }
-        */
-
-        m_eventExecutor->addEvent(std::shared_ptr<Common::EventInterface>(new UartTxEvent("Uart test!  My name is seth and I am cool!", m_uart)));
-
-        while(true);
-
     }
     catch (const std::runtime_error &e) {
         m_output->writeLine(e.what());
