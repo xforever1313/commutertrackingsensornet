2bd3ca4839bf17ec36a522f5c186bb7e77e36039
Seth Hendrick <xforever1313@aim.com>
2014-09-27 15:13:14 -0400

    Added auth to the website.
    Theres not cookies or stuff like that, but oh well.

--- website/ctsn/urls.py
+++ website/ctsn/urls.py
@@ -11,8 +11,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'ctsn_web.views.HomeView'),
     url(r'^index.html$', 'ctsn_web.views.HomeView'),
-    url(r'admin_messages.html', 'ctsn_web.views.AdminMessageView'),
-    url(r'node_status.html', 'ctsn_web.views.NodeStatusView'),
-    url(r'node_stats.html', 'ctsn_web.views.NodeStatsView'),
-    url(r'admin_maintenance.html', 'ctsn_web.views.AdminMaintenanceView')
+    url(r'^admin_messages.html$', 'ctsn_web.views.AdminMessageView'),
+    url(r'^node_status.html$', 'ctsn_web.views.NodeStatusView'),
+    url(r'^node_stats.html$', 'ctsn_web.views.NodeStatsView'),
+    url(r'^admin_maintenance.html$', 'ctsn_web.views.AdminMaintenanceView'),
+    url(r'^login.html$', 'ctsn_web.views.LoginRequest'),
+    url(r'^logout.html$', 'ctsn_web.views.LogoutRequest')
 )
--- website/ctsn_web/views.py
+++ website/ctsn_web/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
 from ctsn_web.models import *
+from ctsn_web.forms import *
 
 NavBarURLs = [('Home', 'index.html'), ('Node Status', 'node_status.html'),
               ('Statistics', 'node_stats.html'),
@@ -11,11 +14,26 @@ AdminBarURLs = [('Messages', 'admin_messages.html'),
               ('Maintenance', 'admin_maintenance.html')]
 
 def HomeView(request):
-    context = { 'NavBarURLs' : NavBarURLs, 'pageID' : NavBarURLs[0][1], 
+    #If not authenticated, return to login
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('login.html')
+
+    if request.user.is_staff:
+        urls = NavBarURLs
+    else:
+        urls = NavBarURLs[:-1]
+
+    context = { 'NavBarURLs' : urls, 'pageID' : NavBarURLs[0][1], 
                 'title' : 'CTSN Home' }
     return render_to_response(NavBarURLs[0][1], context, context_instance=RequestContext(request))
 
 def AdminMessageView(request):
+    #If not authenticated, return to login
+    if not request.user.is_authenticated():
+       return HttpResponseRedirect('login.html')
+    elif not request.user.is_staff:
+        return HttpResponse('Unauthorized', status=401)
+    
     errorMessages = ErrorLog.objects.all()
     context = { 'NavBarURLs' : NavBarURLs, 'pageID' : NavBarURLs[3][1],
                 'title' : 'CTSN Trail Node Log', 'AdminBarURLs' : AdminBarURLs,
@@ -24,23 +42,80 @@ def AdminMessageView(request):
     return render_to_response(NavBarURLs[3][1], context, context_instance=RequestContext(request))
 
 def NodeStatusView(request):
-    context = {'NavBarURLs' : NavBarURLs, 'pageID' : NavBarURLs[1][1], 
+    #If not authenticated, return to login
+    if not request.user.is_authenticated():
+       return HttpResponseRedirect('login.html')
+
+    if request.user.is_staff:
+        urls = NavBarURLs
+    else:
+        urls = NavBarURLs[:-1] #Do not show admin link.
+
+    context = {'NavBarURLs' : urls, 'pageID' : NavBarURLs[1][1], 
                'title' : 'CTSN Node Status',
                'nodes' : Node.objects.all()}
 
     return render_to_response(NavBarURLs[1][1], context, context_instance=RequestContext(request))
 
 def NodeStatsView(request):
-    context = {'NavBarURLs' : NavBarURLs, 'pageID' : NavBarURLs[2][1], 
+    #If not authenticated, return to login
+    if not request.user.is_authenticated():
+       return HttpResponseRedirect('login.html')
+
+    if request.user.is_staff:
+        urls = NavBarURLs
+    else:
+        urls = NavBarURLs[:-1] #Do not show admin link.
+
+    context = {'NavBarURLs' : urls, 'pageID' : NavBarURLs[2][1], 
                'title' : 'CTSN Statistics',
                'nodes' : Node.objects.all()}
 
     return render_to_response(NavBarURLs[2][1], context, context_instance=RequestContext(request))
 
 def AdminMaintenanceView(request):
+    #If not authenticated, return to login
+    if not request.user.is_authenticated():
+       return HttpResponseRedirect('login.html')
+    elif not request.user.is_staff:
+        return HttpResponse('Unauthorized', status=401)
+
     context = {'NavBarURLs' : NavBarURLs, 'pageID' : AdminBarURLs[1][1], 
                'title' : 'CTSN Node Maintenance', 'AdminBarURLs' : AdminBarURLs,
                'nodes' : Node.objects.all()}
 
     return render_to_response(AdminBarURLs[1][1], context, context_instance=RequestContext(request))
+
+###
+# Auth Stuff
+###
+def LoginRequest(request):
+    #If already autheticated, return to home
+    if request.user.is_authenticated():
+       return HttpResponseRedirect('/')
+   
+    #If submitting the form...
+    if (request.method == 'POST'):
+        form = LoginForm(request.POST)
+        if (form.is_valid()):
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            ctsnUser = authenticate(username = username, password=password)
+            if ctsnUser is not None:
+                login(request, ctsnUser)
+                return HttpResponseRedirect('/') 
+            else:
+                return render_to_response('login.html', {'form':form}, context_instance=RequestContext(request))
+
+        else: #Form invalid
+            return render_to_response('login.html', {'form':form}, context_instance=RequestContext(request))
+
+    else: #User is not submitting form, but get requesting.  Show the form
+        form = LoginForm()
+        context = {'form' : form}
+        return render_to_response('login.html', context, context_instance=RequestContext(request)) 
+
+def LogoutRequest(request):
+    logout(request)
+    return HttpResponseRedirect('login.html')
 
