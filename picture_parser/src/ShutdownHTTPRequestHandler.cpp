f96554a68e27c3469295237637cbdc8c2c1c9453
Seth Hendrick <xforever1313@aim.com>
2014-11-20 17:58:38 -0500

    Added a shutdown http request handler to the new picture parser.

---
+++ picture_parser/src/ShutdownHTTPRequestHandler.cpp
@@ -0,0 +1,46 @@
+#include <Poco/Exception.h>
+#include <Poco/Net/HTMLForm.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+
+#include "ctsn_common/ShutdownInterface.h"
+#include "picture_parser/ShutdownHTTPRequestHandler.h"
+
+namespace PictureParser {
+
+const std::string ShutdownHTTPRequestHandler::POST_TRUE_MESSAGE = "Shutting down picture parser...";
+const std::string ShutdownHTTPRequestHandler::POST_FALSE_MESSAGE = "Picture Parser will not shutdown.";
+const std::string ShutdownHTTPRequestHandler::GET_MESSAGE = "Form data:\n\tshutdown=true\tShut down the picture parser";
+const std::string ShutdownHTTPRequestHandler::SHUTDOWN_FORM_DATA = "shutdown";
+
+ShutdownHTTPRequestHandler::ShutdownHTTPRequestHandler(CTSNCommon::ShutdownInterface *shutdown) :
+    m_shutdown(shutdown)
+{
+}
+
+ShutdownHTTPRequestHandler::~ShutdownHTTPRequestHandler() {
+}
+
+void ShutdownHTTPRequestHandler::handlePostRequest(Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response) {
+
+    try {
+        Poco::Net::HTMLForm form(request, request.stream());
+        if (form[SHUTDOWN_FORM_DATA] == "true") {
+            m_shutdown->shutdown();
+            sendSuccessResponse(response, POST_TRUE_MESSAGE);
+        }
+        else {
+            sendSuccessResponse(response, POST_FALSE_MESSAGE);
+        }
+    }
+    catch (const Poco::NotFoundException &e) {
+        sendBadRequestResponse(response);
+    }
+}
+
+void ShutdownHTTPRequestHandler::handleGetRequest(Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response) {
+    sendSuccessResponse(response, GET_MESSAGE);
+}
+
+}
+
