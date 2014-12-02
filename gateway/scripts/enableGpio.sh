819eaacd5b3b6c2ed6372107c18bc16f74f721a8
Seth Hendrick <xforever1313@aim.com>
2014-12-01 22:24:03 -0500

    Fixed the gpio enable script for the pi

--- gateway/scripts/enableGpio.sh
+++ gateway/scripts/enableGpio.sh
@@ -1,4 +1,4 @@
-echo "23" > sys/class/gpio/export
-echo "out" > sys/class/gpio/gpio23/direction 
-chmod 666 sys/class/gpio/gpio23/value
+echo "23" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio23/direction 
+chmod 666 /sys/class/gpio/gpio23/value
 
