8b7b19c829573adde4bab8da984b614ee3dcf5b7
Seth Hendrick <xforever1313@aim.com>
2015-01-31 16:25:02 -0500

    Added a script which turns on the pi node's led

---
+++ pi_node/scripts/enableGPIO.sh
@@ -0,0 +1,11 @@
+### Camera interupt pin ###
+#echo "23" > /sys/class/gpio/export
+#echo "in" > /sys/class/gpio/gpio23/direction 
+#chmod 666 /sys/class/gpio/gpio23/value
+
+
+### Status LED ###
+echo "21" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio21/direction
+chmod 666 /sys/class/gpio/gpio21/value
+
