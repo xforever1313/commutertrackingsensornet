e28d58791691b40cd36b5084c3fbc4fda3b20f99
Seth Hendrick <xforever1313@aim.com>
2015-02-14 22:55:41 -0500

    Added fitnesse and cslim.
    We are ready for automated tests!

---
+++ test/fixtures/cslim/fixtures/Fixtures.c
@@ -0,0 +1,10 @@
+#include "Fixtures.h"
+
+SLIM_FIXTURES
+	SLIM_FIXTURE(Division)
+	SLIM_FIXTURE(Count)
+	SLIM_FIXTURE(EmployeePayRecordsRow)
+	SLIM_FIXTURE(ExceptionsExample)
+    SLIM_FIXTURE(Multiplication)
+SLIM_END
+
---
+++ test/fixtures/src/HelloWorld.cpp
@@ -0,0 +1,42 @@
+#include <string>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ctsn_autotest/CTSNFixtures.h"
+#include "SlimList.h"
+
+class HelloWorldFixture {
+    public:
+        HelloWorldFixture() {
+
+        }
+
+        ~HelloWorldFixture() {
+
+        }
+};
+
+void *HelloWorld_Create(StatementExecutor *errorHandler, SlimList *args) {
+    return new HelloWorldFixture();
+}
+
+void HelloWorld_Destroy(void *voidSUT) {
+    HelloWorldFixture *sut = reinterpret_cast<HelloWorldFixture*>(voidSUT);
+    delete sut;
+}
+
+static char *tryThings(void *voidSUT, SlimList *args) {
+    static std::string s = "ok";
+    return &s[0];
+}
+
+SLIM_CREATE_FIXTURE(HelloWorld)
+    SLIM_FUNCTION(tryThings)
+SLIM_END
+
+#ifdef __cplusplus
+}
+#endif
+
