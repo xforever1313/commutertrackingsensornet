d024849fd5b2bf0f99d172db8544d9f3fff8e534
Seth Hendrick <seth@shendrick.net>
2015-02-19 21:46:33 -0500

    Updated the pi cam python script.

--- pi_node/scripts/pi_cam_run.py
+++ pi_node/scripts/pi_cam_run.py
@@ -1,29 +1,63 @@
+#!/usr/bin/python
+
 import RPi.GPIO as GPIO
+import argparse
 import picamera
 import os
 import subprocess
 import time
+import signal
 import sys
 GPIO.setmode(GPIO.BCM)
 
-sys.path.append('../')
-from Secrets import *
+userAgent = ""
+port = "80"
+uri = "/picture_parse"
 
-# GPIO 23 set up as input. It is pulled up to stop false signals
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-def my_callback(channel):
+def gpio_callback(channel):
     with picamera.PiCamera() as camera:
         camera.resolution = (320, 240)
         time.sleep(2)
         pic = 'pic' + time.strftime("%m_%d_%Y_%H_%M_%S") + '.jpg'
         camera.capture(pic, 'jpeg', True)
 
-        subprocess.call(['curl', '-X', 'POST', '-A', PI_NODE_USER_AGENT,
+        subprocess.call(['curl', '-X', 'POST', '-A', userAgent,
                          '--data', 'picture_file=' + os.path.abspath('./' + pic),
-                         'http://localhost:9011/picture_send'])
+                         'http://localhost:' + port + uri])
 
-GPIO.add_event_detect(23, GPIO.RISING, callback=my_callback, bouncetime=200) 
+def signal_handler(signal, frame):
+    print ("Exiting Pi Cam Script...")
+    sys.exit(0)
 
-while True:
-    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--user_agent",
+                        type=str,
+                        dest = "user_agent",
+                        help="The user agent when posting.")
+
+    parser.add_argument("--uri",
+                        type=str,
+                        dest='uri',
+                        help="The uri")
+
+    parser.add_argument("--port",
+                        type=str,
+                        dest = "port",
+                        help="The port to post to.")
+
+    args = parser.parse_args()
+
+    userAgent = args.user_agent
+    uri = args.uri
+    port = args.port
+
+    #GPIO 23 set up as input. It is pulled up to stop false signals
+    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(23, GPIO.RISING, callback=gpio_callback, bouncetime=200)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print ("Running Pi Cam Script...")
+    signal.pause()
